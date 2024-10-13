9111ee6374d55b1932677d29c9cc20223d235388
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-13 22:14:31 +0900

    getting item from array

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const mon = "monday";
-const tue = "tuesday";
-const wed = "wednesday";
-const thu = "thursday";
-const fri = "firday";
-const sat = "saturday";
-const sun = "sunday";
+const dayOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-const dayOfWeek = [mon, tue, wed, thu, fri, sat, sun];
-
-const nonsense = [1, 2, "hello", false, null, true, undefined, "jihye"];
-console.log(dayOfWeek, nonsense);
+console.log(dayOfWeek[4]);
