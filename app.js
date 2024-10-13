d825b4db44a0f50694870b1922a08ba4860934c8
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-13 22:05:24 +0900

    before using array

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
-const amIFat = null;
-let something;
-console.log(something);
+const mon = "monday";
+const tue = "tuesday";
+const wed = "wednesday";
+const thu = "thursday";
+const fri = "firday";
+const sat = "saturday";
+const sun = "sunday";
