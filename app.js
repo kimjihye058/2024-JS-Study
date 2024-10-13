ec0ff099f461a8a67000506b702850ada1ce55f8
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 00:46:25 +0900

    how to make a parameters in function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-console.log("Hello my name is Jihye");
-console.log("Hello my name is Nico");
-console.log("Hello my name is Jennifer");
-console.log("Hello my name is Kady");
-console.log("Hello my name is Shigatsu");
-console.log("Hello my name is Lia");
+function sayHello(nameOfPerson) {
+    console.log(nameOfPerson);
+}
+
+sayHello("Jihye");
+sayHello("Nihye");
+sayHello("Jennifer");
