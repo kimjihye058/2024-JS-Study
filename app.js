7bb3370f503ced9ad4fd41d2f2a533070d9f671c
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-13 21:23:14 +0900

    boolean type

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-const a = 5;
-const b = 2;
-let myName = "jihye";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "jennifer";
-console.log("your name is " + myName);
+const amIFat = true;
+console.log(amIFat);
