251a26bdcf39f7d879e12a0e884e6915a734b0e9
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 00:38:32 +0900

    before using function

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-const player = {
-    name: "jihye", 
-    points: 10,
-    fat: true,
-};
-console.log(player);
-player.lastName = "Kim";
-player.points = player.points + 15;
-console.log(player.points);
+console.log("Hello my name is Jihye");
+console.log("Hello my name is Nico");
+console.log("Hello my name is Jennifer");
+console.log("Hello my name is Kady");
+console.log("Hello my name is Shigatsu");
+console.log("Hello my name is Lia");
