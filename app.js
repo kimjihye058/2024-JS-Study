148be30deabe3fe8b12a0afb3c45f9a20ee7f891
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-13 22:09:24 +0900

    how to use array

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const thu = "thursday";
 const fri = "firday";
 const sat = "saturday";
 const sun = "sunday";
+
+const dayOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+const nonsense = [1, 2, "hello", false, null, true, undefined, "jihye"];
+console.log(dayOfWeek, nonsense);
