39dd349e08d4c3c959e39a658eacc6f4cb9b6d04
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 23:49:24 +0900

    AND(&&) operator

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const age = parseInt( prompt("How old are you?") );
 // NaN이 console에 나온다면 Not a Number임(= 숫자가 아님)
 
-console.log(isNaN(age));
-// isNaN: 값이 숫자인지 아닌지 boolean으로 나타냄
-// 숫자라면 false, 숫자가 아니라면 true
+if(isNaN(age)) {
+    //condition === true
+    console.log("Please write a number.");
+} else if(age < 18) {
+    console.log("You are too young.");
+} else if(age >= 18 && age <= 50) {
+    console.log("You can drink.");
+} else {
+    console.log("You can drink.");
+}
