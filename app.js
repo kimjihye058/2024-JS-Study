e9777c79a6408e1f12283a6966af57e9a29f9701
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 23:51:27 +0900

    OR(||) operator

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ if(isNaN(age)) {
     console.log("Please write a number.");
 } else if(age < 18) {
     console.log("You are too young.");
-} else if(age >= 18 && age <= 50) {
+} else if(age >= 18 || age <= 50) {
     console.log("You can drink.");
 } else {
     console.log("You can drink.");
