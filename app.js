4e63fcdc7c90f035222ef314a09bc738c8b03880
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 22:33:12 +0900

    return value

--- app.js
+++ app.js
@@ -1,23 +1,8 @@
-const calculator={
-    plus: function (a,b){
-        console.log(a+b);
-    },
-    minus: function (a,b){
-        console.log(a-b);
-    },
-    times: function (a,b){
-        console.log(a*b);
-    },
-    divide: function (a,b){
-        console.log(a/b);
-    },
-    power: function (a,b){
-        console.log(a**b);
-    },
-};
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner+2;
+}
 
-calculator.plus(2,3);
-calculator.minus(2,3);
-calculator.times(2,3);
-calculator.divide(2,3);
-calculator.power(2,3);
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
