43ed8304fe96d64875b2d5bcf453b878fb20fdf0
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 23:22:10 +0900

    typeof + parseInt

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 const age = prompt("How old are you?");
 
-parseInt("15"); // conert string type to int type
-
-console.log(typeof age);
+console.log(typeof "15", typeof parseInt("15"));    // string number
