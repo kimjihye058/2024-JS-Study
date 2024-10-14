5b97e55baa621845c1415ad1d1b767baf96c9c00
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 23:29:40 +0900

    isNaN

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-const age = prompt("How old are you?");
+const age = parseInt( prompt("How old are you?") );
+// NaN이 console에 나온다면 Not a Number임(= 숫자가 아님)
 
-console.log(typeof "15", typeof parseInt("15"));    // string number
+console.log(isNaN(age));
+// isNaN: 값이 숫자인지 아닌지 boolean으로 나타냄
+// 숫자라면 false, 숫자가 아니라면 true
