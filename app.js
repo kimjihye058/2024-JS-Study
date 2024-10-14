008c12d28548ce8684e5f7a93b2e0dc975b9f938
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-14 22:41:35 +0900

    After using return

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const age = 96;
-function calculateKrAge(ageOfForeigner){
-    return ageOfForeigner+2;
+const calculator = {
+    plus: function(a, b){
+        return (a+b);
+    }, 
+    minus: function(a, b){
+        return (a-b);
+    },
+    times: function(a, b){
+        return (a*b);
+    }, 
+    divide: function(a, b){
+        return (a/b);
+    },
+    power: function(a, b){
+        return (a**b);
+    }, 
 }
 
-const krAge = calculateKrAge(age);
+const plusResult = calculator.plus(2, 3);
+const minusResult = calculator.minus(2, 3);
+const timesResult = calculator.times(2, 3);
+const divideResult = calculator.divide(2, 3);
+const powerResult = calculator.power(2, 3);
 
-console.log(krAge);
+console.log(plusResult);
+console.log(minusResult);
+console.log(timesResult);
+console.log(divideResult);
+console.log(powerResult);
