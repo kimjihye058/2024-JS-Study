6113223458ce994b2c756d00e629c333be3f5669
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-15 00:01:31 +0900

    difference between OR and AND

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const age = parseInt( prompt("How old are you?") );
 // NaN이 console에 나온다면 Not a Number임(= 숫자가 아님)
 
-if(isNaN(age)) {
+if (isNaN(age)  || age < 0) {
     //condition === true
-    console.log("Please write a number.");
-} else if(age < 18) {
+    console.log("Please write a real positive number.");
+} else if (age < 18) {
     console.log("You are too young.");
-} else if(age >= 18 || age <= 50) {
+} else if (age >= 18 && age <= 50) {
     console.log("You can drink.");
-} else {
-    console.log("You can drink.");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise.");
+} else if (age > 80) {
+    console.log("You can do whatever you want!");
 }
+
+// OR(||) operator
+// true || true === true
+// false || true === true
+// true || false === true
+// false || false === false
+
+// AND(&&) operator
+// true && true === true
+// false && true === false
+// true && false === false
+// false && false === false
