240268e182f7770e390b484e5fddafce0c3bca69
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-26 00:19:35 +0900

    querySelector return only one

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const title = document.querySelector(".hello h1");
 // querySelector may search for an element in a css method.
+// querySelector returns only one element.
+// ** querySelector brings only the first element.
 
-console.log(title);     // title is an array
+console.log(title);
