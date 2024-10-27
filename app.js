ecca6d578c62cb2005004ca8c88feae171ec44d0
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-27 23:42:00 +0900

    handle windowResize

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() { //When a click event occurs
-    title.style.color = "blue";
+    h1.style.color = "blue";
 } 
 
 function handleMouseEnter() { //When the mouse touches h1
-    title.innerText ="Mouse is here!";
+    h1.innerText ="Mouse is here!";
 }
 
 function handleMouseLeave() { //when the mouse is gone from h1
-    title.innerText = "Mouse is gone!";
+    h1.innerText = "Mouse is gone!";
 }
 
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+
