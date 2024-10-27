095db68fc3a855343f5b8d40297df20af24a8b25
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-28 00:25:53 +0900

    Creating and deleting className using if-else

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    h1.className = "active"; // .active of css
+    if(h1.className === "active") {
+        h1.className = "";
+    } else{
+        h1.className = "active";
+    }
 }
 
 h1.addEventListener("click", handleTitleClick);
