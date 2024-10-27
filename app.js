582fa58c22866f5e70be52aa89c59b5c44d45e7c
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-28 00:47:54 +0900

    toggle; checking wheter the class name exists

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)) { //classList: Allows for manipulation of element's class name
-        h1.classList.remove(clickedClass);
-    } else{
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle("clicked"); // toggle function checks whether the class name exists
 }
 
 h1.addEventListener("click", handleTitleClick);
