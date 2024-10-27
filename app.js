a24b9e30cc4e19489690f16850becae1365132bc
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-28 00:18:05 +0900

    if-else with click eventer(easy to read code ver.)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    if(h1.style.color ===  "blue"){ // In === case, make sure that the values on the right and left sides match.
-        h1.style.color = "tomato";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor ===  "blue"){ // In === case, make sure that the values on the right and left sides match.
+        newColor = "tomato";
     } else{
-        h1.style.color = "blue";
+        newColor = "blue";
     }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
