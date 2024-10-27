1a04faf1161975348b9d1ba3a0be3d4d29df7803
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-28 00:41:54 +0900

    ClassString changes class altogether.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "clicked sexy-font";
-    if (h1.className === clickedClass) {
-        h1.className = "";
+    const clickedClass = "clicked";
+    if(h1.classList.contains(clickedClass)) { //classList: Allows for manipulation of element's class name
+        h1.classList.remove(clickedClass);
     } else{
-        h1.className = clickedClass;
+        h1.classList.add(clickedClass);
     }
 }
 
