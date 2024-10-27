91eaed4757055c347ec262831b4dc64b657afd58
kimjihye058 <s2321@e-mirim.hs.kr>
2024-10-28 00:15:27 +0900

    if-else with click eventer

--- app.js
+++ app.js
@@ -1,38 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-function handleTitleClick() { //When a click event occurs
-    h1.style.color = "blue";
-} 
-
-function handleMouseEnter() { //When the mouse touches h1
-    h1.innerText ="Mouse is here!";
-}
-
-function handleMouseLeave() { //when the mouse is gone from h1
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() { //When adjusting the window screen
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() { //When copying something (ctrl+c)
-    alert("copier!");
-}
-
-function handleWindowOffline() { //When the Wi-Fi is not connected
-    alert("Wi-Fi not connected");
-}
-
-function handleWindowOnline() { //When the Wi-Fi is connected
-    alert("Wi-Fi connected");
+function handleTitleClick() {
+    if(h1.style.color ===  "blue"){ // In === case, make sure that the values on the right and left sides match.
+        h1.style.color = "tomato";
+    } else{
+        h1.style.color = "blue";
+    }
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
