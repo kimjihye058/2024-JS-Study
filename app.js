d82c6e80ee6ffdbfd0224bb3f25b75372a58ef1e
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-06 12:59:28 +0900

    Form Submission

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const loginButton = document.querySelector("#login-form button");
 
 function LoginBtnClick() {
     const username = loginInput.value;
-    if(username === ""){
-        alert("Please write your name.");
-    }else if(username.length > 15){
-        alert("Your name is too long.")
-    }
+    console.log(username);
 }
 
 loginButton.addEventListener("click", LoginBtnClick);
