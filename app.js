bb1699a1bd05ba0adc2910310fd476e0c5bbb415
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-06 12:48:09 +0900

    click event with loginInput.value

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function LoginBtnClick() {
-    console.log(loginInput.value);        // click시 value가 저장됨
-    console.log("click");       // click시 "click"이라고 console에 찍음
+    console.log("Hello,", loginInput.value);
 }
 
 loginButton.addEventListener("click", LoginBtnClick);
