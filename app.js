86bd0c8bb6c3ab8058af4952ef1f89305da41fea
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-07 13:06:17 +0900

    Saving username in the localStorage

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden"; //Variables that contain only strings are capitalized
 
 function onLoginSubmit(event) {
-    event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    const username = loginInput.value;
-    greeting.innerText = "Hello " + username;   // combining Strings 1
+    event.preventDefault();     // Prevent default behavior in browser
+    loginForm.classList.add(HIDDEN_CLASSNAME);  // add calss 'hidden' in form
+    const username = loginInput.value;      
+    localStorage.setItem("username", username); // saving Username
+    // greeting.innerText = "Hello " + username;   // combining Strings 1
     greeting.innerText = `Hello ${username}`;   // combining Strings 2
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
