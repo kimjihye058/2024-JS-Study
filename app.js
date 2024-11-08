f84403018825a189bffc8e543cad0ea49a29e4e9
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-08 23:41:34 +0900

    Loading Username

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
-const HIDDEN_CLASSNAME = "hidden"; //Variables that contain only strings are capitalized
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-    event.preventDefault();     // Prevent default behavior in browser
-    loginForm.classList.add(HIDDEN_CLASSNAME);  // add calss 'hidden' in form
+    event.preventDefault();   
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
     const username = loginInput.value;      
-    localStorage.setItem("username", username); // saving Username
-    // greeting.innerText = "Hello " + username;   // combining Strings 1
-    greeting.innerText = `Hello ${username}`;   // combining Strings 2
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`; // Add text to greeting
+    greeting.classList.remove(HIDDEN_CLASSNAME);    // remove HIDDEN_CLASSNAME to greeting
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings(savedUsername);
+}
