4ff394f538dc9bbae72c233d8ca24fab9a2b874b
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-09 14:32:19 +0900

    randomness Quotes

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,7 @@ const clock = document.querySelector("#clock");
 
 function getClock() {
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2, "0");     // Be at least 2 characters long otherwise add "0" before
+    const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText =`${hours}:${minutes}:${seconds}`;
