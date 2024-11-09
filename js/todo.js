a11ba9b6fb1525a854ec04df85eceabdf8df15ff
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-10 03:58:18 +0900

    adding Todos

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+    const li = document.createElement("li");        // create li
+    const span = document.createElement("span");    // create span
+    li.appendChild(span);       // Put span as child inside li
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
-    const newTodo = toDoInput.value;        // Copy the current value of the input to a new variable
+    const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
