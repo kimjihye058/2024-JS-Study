ab86e14cd5e434a8e89dd5733a861968db5b0ae4
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-09 13:55:50 +0900

    Making a Clock

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,9 @@
 const clock = document.querySelector("#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date();
+    clock.innerText =`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-setTimeout(sayHello, 5000);     // Run sayHello function in 5 seconds
+getClock();     // time came out right away
+setInterval(getClock, 1000);
