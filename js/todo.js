663bd34332b1c1ebc73c6a9ebc80cebfdca9bd9a
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-13 10:18:01 +0900

    Options to store id in the database

--- js/todo.js
+++ js/todo.js
@@ -12,13 +12,15 @@ function saveToDos(){
 
 function deleteToDo(event){
     const li = event.target.parentElement;
+    console.log(li.id);
     li.remove();
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -36,7 +38,7 @@ function handleToDoSubmit(event) {
         id: Date.now(),
     }
     toDos.push(newTodoObj);
-    paintToDo(newTodo);
+    paintToDo(newTodoObj);
     saveToDos(); 
 }
 
