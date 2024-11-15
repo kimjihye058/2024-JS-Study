502e51282ce66f7ee793641f8e62ad19573fe4c5
kimjihye058 <s2321@e-mirim.hs.kr>
2024-11-15 20:21:05 +0900

    get url(weather api)

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,10 @@
+import CONFIG from './config.js';
+
 function onGeoOk(position){
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${CONFIG.API_KEY}`;
+    fetch(url);
 }
 function onGeoError(){
     alert("Can't find you! No weather for you...");
